5c5771d3fb2af30b314aaca6903c0fec01ab9f16
A-2574 <A-2574@192.168.1.151>
2019-01-31 18:13:38 +0530

    31-01-2019 changes

--- SampleApp/WebContent/app.js
+++ SampleApp/WebContent/app.js
@@ -14,7 +14,7 @@ var isOpera = typeof window.opr !== "undefined";
 var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
 var isIOSChrome = winNav.userAgent.match("CriOS");
 
-if (isIOSChrome) {
+/*if (isIOSChrome) {
 	recordButton.addEventListener("click", startRecognitionChrome);
 	console.log("Browser is ios chrome")
 } else if(
@@ -29,8 +29,8 @@ if (isIOSChrome) {
 } else { 
 	recordButton.addEventListener("click", startRecording);
 	console.log("Browser is not chrome")
-}
-
+}*/
+recordButton.addEventListener("click", startRecording);
 function startRecording() {
 	console.log("recordButton clicked");
      var constraints = { audio: true, video:false }
@@ -95,7 +95,7 @@ function typeSearchByString(text){
 	 // //var urlValue = base64data;
 	  fd.append("searchString",text);
 	 //fd.append("audio_data",blob);
-	  xhr.open("POST","./record",true);
+	  xhr.open("POST","./witapi",true);
 	  xhr.send(fd);
 }
 
@@ -148,9 +148,9 @@ function createDownloadLink(blob) {
 		  var fd=new FormData();
 		 // var vals=$("#test").attr('src');
 		 // var urlValue = base64data;
-		  fd.append("exmple","555");
-		 fd.append("audio_data",blob);
-		  xhr.open("POST","./record",true);
+		  //fd.append("exmple","555");
+		 fd.append("nonchromeFlag","true");
+		  xhr.open("POST","./witapi",true);
 		  xhr.send(blob);
 		  
 	}, 1000);
--- SampleApp/src/com/flightsearch/witapi/WitServlet.java
+++ SampleApp/src/com/flightsearch/witapi/WitServlet.java
@@ -2,9 +2,12 @@ package com.flightsearch.witapi;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 import javax.servlet.ServletException;
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
 @MultipartConfig
 public class WitServlet extends HttpServlet {
 	@Override
@@ -20,14 +25,24 @@ public class WitServlet extends HttpServlet {
 		req.setCharacterEncoding("UTF-8");
 		String searchString=req.getParameter("searchString");
 		String languageofApp=req.getParameter("languageofApp");
-		getApiResponse(searchString,languageofApp);
+		 //String nonchromeFlag=req.getParameter("nonchromeFlag");
+		InputStream input = req.getInputStream(); 
+       
+		 if(input.available()>0) {
+			 byte[] bytes = IOUtils.toByteArray(input); 
+			getApiTextResponsefromWav(bytes, languageofApp);
+			 
+		 }else {
+			 getApiTextResponse(searchString,languageofApp);
+		 }
+		
 		
 		super.doPost(req, resp);
 	}
 
 	
 	
-	public static void getApiResponse(String searchString,String languageofApp) throws IOException {
+	public static void getApiTextResponse(String searchString,String languageofApp) throws IOException {
 	    String url = "https://api.wit.ai/message?v=20190130&q="+searchString;
 	    String key="";
 	    if(languageofApp.equals("JA")){
@@ -43,5 +58,44 @@ public class WitServlet extends HttpServlet {
 	    while((line = response.readLine()) != null) {
 	        System.out.println(line);
 	    }
+	}
+	    public static void getApiTextResponsefromWav(byte[] searchString,String languageofApp) throws IOException {
+	    	String url = "https://api.wit.ai/speech";
+	    	String key="";
+	    	 if(languageofApp.equals("JA")){
+	    		    key = "HYRE3CGXIV2JUBZLXQOUXOT4GFHPCREE";
+	    		    }else{
+	    		    	 key = "CO25MYLYBZ6RA5PU2TSALWXHNKTNOLJ3";	
+	    		    }
+
+	        String param1 = "20170203";
+	        String param2 = "command";
+	        String charset = "UTF-8";
+
+	        String query = String.format("v=%s",
+	                URLEncoder.encode(param1, charset));
+
+
+	        URLConnection connection = new URL(url + "?" + query).openConnection();
+	        connection.setRequestProperty ("Authorization","Bearer " + key);
+	        connection.setRequestProperty("Content-Type", "audio/wav");
+	        connection.setDoOutput(true);
+	        OutputStream outputStream = connection.getOutputStream();
+	        outputStream.write(searchString);
+		/*
+		 * OutputStream outputStream = connection.getOutputStream(); FileChannel
+		 * fileChannel = new FileInputStreamÅipath to sample.wav).getChannel();
+		 * ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+		 * 
+		 * while((fileChannel.read(byteBuffer)) != -1) { byteBuffer.flip(); byte[] b =
+		 * new byte[byteBuffer.remaining()]; byteBuffer.get(b); outputStream.write(b);
+		 * byteBuffer.clear(); }
+		 */
+
+	        BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        String line;
+	        while((line = response.readLine()) != null) {
+	            System.out.println(line);
+	        }
 	}
 }
